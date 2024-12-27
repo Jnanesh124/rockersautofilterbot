5a6a4c4a84bc1c1e9799f42437469a4f1bc6fb9a
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-12-27 17:13:38 +0600

    Update Script.py

--- Script.py
+++ Script.py
@@ -104,7 +104,7 @@ User - {}"""
 ❗️ ʟᴀɴɢᴜᴀɢᴇ - {languages}</b>
 """
 
-    FILE_CAPTION = "File Name: {file_name}, File Size: {file_size}"
+    FILE_CAPTION = "File Name: {file_name}, File Size: {file_size}, File Caption: {file_caption}"
 
     RESTART_TXT = """<b>
 📅 Dᴀᴛᴇ : <code>{}</code>
