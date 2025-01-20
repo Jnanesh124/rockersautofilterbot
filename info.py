679918a769957ed8775c72d9b85e088153f8f9bf
JN2FLIX <99884442+Jnanesh124@users.noreply.github.com>
2025-01-20 15:05:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 USERNAME = environ.get('USERNAME', "https://telegram.me/ROCKERSBACKUP")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002082139065'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+RowdIa84tBg1MDI1')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002159172109').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002449650319').split()]
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://test:test@cluster0.ys8igld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "TELEGRAM_BOT_INFO")
